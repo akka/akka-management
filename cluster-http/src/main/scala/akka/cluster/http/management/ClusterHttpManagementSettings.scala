3da1479b96b4758b5f9a12ed3b06dda0f5fac875
sebastian.harko <sebastian.harko@lightbend.com>
2017-03-01 11:50:49 +0100

    default hostname to InetAddress.getLocalHost.getHostAddress() when left empty ""

--- cluster-http/src/main/scala/akka/cluster/http/management/ClusterHttpManagementSettings.scala
+++ cluster-http/src/main/scala/akka/cluster/http/management/ClusterHttpManagementSettings.scala
@@ -3,10 +3,18 @@
  */
 package akka.cluster.http.management
 
+import java.net.InetAddress
+
 import com.typesafe.config.Config
 
 final class ClusterHttpManagementSettings(val config: Config) {
   private val cc = config.getConfig("akka.cluster.http.management")
   val ClusterHttpManagementPort = cc.getInt("port")
-  val ClusterHttpManagementHostname = cc.getString("hostname")
+  val ClusterHttpManagementHostname = {
+    val hostname = cc.getString("hostname")
+    if (hostname == "")
+      InetAddress.getLocalHost.getHostAddress
+    else
+      hostname
+  }
 }
