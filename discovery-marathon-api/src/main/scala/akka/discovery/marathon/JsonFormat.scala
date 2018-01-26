845f268c543fb2466a0ae37a63880698d820f89b
Yury Gribkov <yury.gribkov@gmail.com>
2018-01-25 20:56:32 -0500

    marathon-api add docker-support (#113)
    * marathon-api add docker-support
    
    * Marathon API discovery example.
    
    * Marathon API discovery example. Simplify example. Convert to sub-project. Simplify build/deploy process

--- discovery-marathon-api/src/main/scala/akka/discovery/marathon/JsonFormat.scala
+++ discovery-marathon-api/src/main/scala/akka/discovery/marathon/JsonFormat.scala
@@ -11,7 +11,8 @@ object JsonFormat extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val taskFormat: JsonFormat[Task] = jsonFormat2(Task)
   implicit val portDefinitionFormat: JsonFormat[PortDefinition] = jsonFormat2(PortDefinition)
   implicit val portMappingFormat: JsonFormat[PortMapping] = jsonFormat2(PortMapping)
-  implicit val containerFormat: JsonFormat[Container] = jsonFormat1(Container)
+  implicit val dockerFormat: JsonFormat[Docker] = jsonFormat1(Docker)
+  implicit val containerFormat: JsonFormat[Container] = jsonFormat2(Container)
   implicit val appFormat: JsonFormat[App] = jsonFormat3(App)
   implicit val appListFormat: RootJsonFormat[AppList] = jsonFormat1(AppList.apply)
 }
