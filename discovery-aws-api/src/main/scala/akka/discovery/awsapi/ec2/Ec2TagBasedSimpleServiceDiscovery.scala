e43554434cb08d7d1679bdcb2d0de533f8458d27
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-04 19:15:13 -0800

    fix bug in the 'other filters' feature

--- discovery-aws-api/src/main/scala/akka/discovery/awsapi/ec2/Ec2TagBasedSimpleServiceDiscovery.scala
+++ discovery-aws-api/src/main/scala/akka/discovery/awsapi/ec2/Ec2TagBasedSimpleServiceDiscovery.scala
@@ -29,15 +29,20 @@ class Ec2TagBasedSimpleServiceDiscovery(system: ActorSystem) extends SimpleServi
     val otherFiltersString =
       system.settings.config.getConfig("akka.discovery.aws-api-ec2-tag-based").getString("filters")
 
-    val otherFilters: util.List[Filter] = otherFiltersString
-      .split(";")
-      .map(kv => kv.split("="))
-      .toList
-      .map(kv => {
-        assert(kv.length == 2, "failed to parse one of the key-value pairs in filters")
-        new Filter(kv(0), List(kv(1)).asJava)
-      })
-      .asJava
+    val otherFilters: util.List[Filter] =
+      otherFiltersString match {
+        case "" => List[Filter]().asJava
+        case value => otherFiltersString
+          .split(";")
+          .map(kv => kv.split("="))
+          .toList
+          .map(kv => {
+            assert(kv.length == 2, "failed to parse one of the key-value pairs in filters")
+            new Filter(kv(0), List(kv(1)).asJava)
+          })
+          .asJava
+      }
+
 
     val request = new DescribeInstancesRequest()
       .withFilters(tagFilter)
