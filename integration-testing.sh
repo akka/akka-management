b1f9e8d1cd60c454b84e39b89a7b33e425e7882e
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-05 19:51:49 -0800

    add integration testing to .travis.yml file

--- integration-testing.sh
+++ integration-testing.sh
@@ -5,15 +5,21 @@ echo "* Publishing akka-management locally   *"
 echo "****************************************"
 
 sbt clean publishLocal
-cd bootstrap-joining-demo/aws-api
+cd bootstrap-joining-demo/aws-api/ec2-tag-based
 
-echo "****************************************"
-echo "* Building aws-api package (app.zip)   *"
-echo "****************************************"
+echo "**********************************************"
+echo "* Building ec2-tag-based package (app.zip)   *"
+echo "**********************************************"
 sbt clean universal:packageBin
 
 echo "****************************************"
 echo "* Uploading app.zip to S3              *"
 echo "****************************************"
 
-aws s3 cp target/universal/app.zip s3://$BUCKET/ --acl public-read   
+aws s3 cp target/universal/app.zip s3://$BUCKET/ --acl public-read  
+
+echo "***************************************************************"
+echo " Running integration tests for ec2-tag-based demo             *"
+echo "***************************************************************"
+
+sbt test
