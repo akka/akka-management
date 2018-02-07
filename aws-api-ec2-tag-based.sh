c960c786777cd2cdc1f7a67c83ef18f3ee582fd7
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-06 19:30:23 -0800

    bash script: create S3 bucket if it doesn't already exist

--- aws-api-ec2-tag-based.sh
+++ aws-api-ec2-tag-based.sh
@@ -1,9 +1,11 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 echo Running integration test for aws-api-ec2-tag-based
 
 sbt clean publishLocal
 cd bootstrap-joining-demo/aws-api/ec2-tag-based
 sbt clean universal:packageBin
+# create bucket if it doesn't already exist
+aws s3api create-bucket --bucket $BUCKET --region us-east-1
 aws s3 cp target/universal/app.zip s3://$BUCKET/ --acl public-read
 sbt test
