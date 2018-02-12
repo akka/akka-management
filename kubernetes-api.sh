83f5eebf7c8be9e45ce8106bf5c08027900d56f0
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-12 13:48:29 -0800

    publish docker image

---
+++ kubernetes-api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo Running integration test for kubernetes-api
+
+docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+sbt "bootstrap-joining-demo-kubernetes-api/docker:publishLocal"
+ 
