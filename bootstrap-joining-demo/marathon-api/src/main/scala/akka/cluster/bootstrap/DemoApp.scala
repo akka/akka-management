3777ea19889ab98293e3eff1f8947fb5051f8d4f
Jason Longshore <longshorej@gmail.com>
2018-02-12 11:50:56 -0600

    Improve marathon demos, adding health/readiness checks

--- bootstrap-joining-demo/marathon-api/src/main/scala/akka/cluster/bootstrap/DemoApp.scala
+++ bootstrap-joining-demo/marathon-api/src/main/scala/akka/cluster/bootstrap/DemoApp.scala
@@ -5,14 +5,41 @@
 package akka.cluster.bootstrap
 
 import akka.actor.ActorSystem
+import akka.cluster.{ Cluster, MemberStatus }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
 import akka.management.AkkaManagement
 import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.stream.ActorMaterializer
 
 object DemoApp extends App {
-
   implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+
+  val cluster = Cluster(system)
+
+  def isReady() = {
+    val selfNow = cluster.selfMember
+
+    selfNow.status == MemberStatus.Up
+  }
+
+  def isHealthy() = {
+    isReady()
+  }
+
+  val route =
+    concat(
+      path("ping")(complete("pong!")),
+      path("healthy")(complete(if (isHealthy()) StatusCodes.OK else StatusCodes.ServiceUnavailable)),
+      path("ready")(complete(if (isReady()) StatusCodes.OK else StatusCodes.ServiceUnavailable)))
 
   AkkaManagement(system).start()
   ClusterBootstrap(system).start()
 
+  Http().bindAndHandle(
+    route,
+    sys.env.get("HOST").getOrElse("127.0.0.1"),
+    sys.env.get("PORT_HTTP").map(_.toInt).getOrElse(8080))
 }
