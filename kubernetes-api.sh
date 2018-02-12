5b76e50587e2a5c77d0d9cdde8d5cb5ce2388de3
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-12 14:24:25 -0800

    integration testing script pushes image to docker hub

--- kubernetes-api.sh
+++ kubernetes-api.sh
@@ -5,4 +5,6 @@ echo Running integration test for kubernetes-api
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 sbt "bootstrap-joining-demo-kubernetes-api/docker:publishLocal"
 docker images
+docker tag akka-management-bootstrap-joining-demo-kubernetes-api:1.3.3.7 sebastianharko/akka-management-bootstrap-joining-demo-kubernetes-api:1.3.3.7
+docker push sebastianharko/akka-management-bootstrap-joining-demo-kubernetes-api:1.3.3.7
  
