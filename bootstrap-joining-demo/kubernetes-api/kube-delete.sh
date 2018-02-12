56013155af41acc0c6b559966ce93973e1419c4c
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-11 17:26:42 -0800

    fix script a bit (less error messages)

--- bootstrap-joining-demo/kubernetes-api/kube-delete.sh
+++ bootstrap-joining-demo/kubernetes-api/kube-delete.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl delete services,pods,deployment -l app=appka
-kubectl delete services,pods,deployment appka-service
+kubectl delete pods,deployment -l app=appka
+kubectl delete services appka-service
