be01c6d558e8b7617a045fd3172dc6b3b879b921
Jason Longshore <longshorej@users.noreply.github.com>
2018-02-13 04:42:56 -0600

    Kubernetes API discovery now ignores pods that are being terminated (#129)

--- discovery-kubernetes-api/src/main/scala/akka/discovery/kubernetes/PodList.scala
+++ discovery-kubernetes-api/src/main/scala/akka/discovery/kubernetes/PodList.scala
@@ -6,11 +6,12 @@ package akka.discovery.kubernetes
 import scala.collection.immutable.Seq
 
 object PodList {
+  case class Metadata(deletionTimestamp: Option[String])
   case class Port(name: String, containerPort: Int)
   case class Container(name: String, ports: Seq[Port])
   case class Spec(containers: Seq[Container])
   case class Status(podIP: Option[String])
-  case class Item(spec: Spec, status: Status)
+  case class Item(spec: Spec, status: Status, metadata: Metadata)
 }
 
 import PodList._
--- discovery-kubernetes-api/src/test/scala/akka/discovery/kubernetes/KubernetesApiSimpleServiceDiscoverySpec.scala
+++ discovery-kubernetes-api/src/test/scala/akka/discovery/kubernetes/KubernetesApiSimpleServiceDiscoverySpec.scala
@@ -13,13 +13,21 @@ class KubernetesApiSimpleServiceDiscoverySpec extends WordSpec with Matchers {
     "calculate the correct list of resolved targets" in {
       val podList = PodList(List(Item(Spec(List(Container("akka-cluster-tooling-example",
                   List(Port("akka-remote", 10000), Port("akka-mgmt-http", 10001), Port("http", 10002))))),
-            Status(Some("172.17.0.4"))),
+            Status(Some("172.17.0.4")), Metadata(deletionTimestamp = None)),
           Item(Spec(List(Container("akka-cluster-tooling-example",
                   List(Port("akka-remote", 10000), Port("akka-mgmt-http", 10001), Port("http", 10002))))),
-            Status(None))))
+            Status(None), Metadata(deletionTimestamp = None))))
 
       KubernetesApiSimpleServiceDiscovery.targets(podList,
         "akka-mgmt-http") shouldBe List(ResolvedTarget("172.17.0.4", Some(10001)))
+    }
+
+    "ignore deleted pods" in {
+      val podList = PodList(List(Item(Spec(List(Container("akka-cluster-tooling-example",
+                  List(Port("akka-remote", 10000), Port("akka-mgmt-http", 10001), Port("http", 10002))))),
+            Status(Some("172.17.0.4")), Metadata(deletionTimestamp = Some("2017-12-06T16:30:22Z")))))
+
+      KubernetesApiSimpleServiceDiscovery.targets(podList, "akka-mgmt-http") shouldBe List.empty
     }
   }
 }
