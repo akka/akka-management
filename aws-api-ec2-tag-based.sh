4e3b53d0febab18e93fd65410241a0fddeea0558
Sebastian Harko <sebastian.harko.bc@gmail.com>
2018-02-14 18:03:39 -0800

    reformat file and fix typo

--- aws-api-ec2-tag-based.sh
+++ aws-api-ec2-tag-based.sh
@@ -4,7 +4,7 @@ echo Running integration test for aws-api-ec2-tag-based
 
 export BUILD_ID=$TRAVIS_JOB_NUMBER-$TRAVIS_JOB_ID
 
-sbt bootstrap-joining-demo-aws-api-ec2-tag-based/universal:packageBin/universal:packageBin
+sbt bootstrap-joining-demo-aws-api-ec2-tag-based/universal:packageBin
 # create bucket if it doesn't already exist
 aws s3api create-bucket --bucket $BUCKET --region us-east-1
 aws s3 cp bootstrap-joining-demo/aws-api/ec2-tag-based/target/universal/app.zip s3://$BUCKET/$BUILD_ID/ --acl public-read
---
+++ bootstrap-joining-demo/aws-api/ec2-tag-based/src/main/scala/akka/cluster/bootstrap/Ec2DemoApp.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.cluster.bootstrap
+
+import akka.actor.ActorSystem
+import akka.management.AkkaManagement
+import akka.management.cluster.bootstrap.ClusterBootstrap
+
+object Ec2DemoApp extends App {
+
+  implicit val system = ActorSystem("demo")
+
+  AkkaManagement(system).start()
+
+  ClusterBootstrap(system).start()
+
+}
