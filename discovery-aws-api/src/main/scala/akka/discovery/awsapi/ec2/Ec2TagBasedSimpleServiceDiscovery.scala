997f610b9a5a82afe0b443258081c74c9c47abd8
sebastianharko <sebastian.harko@lightbend.com>
2018-05-25 22:15:49 -0700

    handle paginated results (aws api)

--- discovery-aws-api/src/main/scala/akka/discovery/awsapi/ec2/Ec2TagBasedSimpleServiceDiscovery.scala
+++ discovery-aws-api/src/main/scala/akka/discovery/awsapi/ec2/Ec2TagBasedSimpleServiceDiscovery.scala
@@ -3,23 +3,23 @@
  */
 package akka.discovery.awsapi.ec2
 
-import java.util
 import java.util.concurrent.TimeoutException
 
 import akka.actor.ActorSystem
 import akka.discovery.SimpleServiceDiscovery
 import akka.discovery.SimpleServiceDiscovery.{ Resolved, ResolvedTarget }
-import akka.discovery.awsapi.ec2.Ec2TagBasedSimpleServiceDiscovery.parseFiltersString
+import akka.discovery.awsapi.ec2.Ec2TagBasedSimpleServiceDiscovery._
 import akka.pattern.after
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.retry.PredefinedRetryPolicies
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
 import com.amazonaws.services.ec2.model.{ DescribeInstancesRequest, Filter, Reservation }
+import com.amazonaws.services.ec2.{ AmazonEC2, AmazonEC2ClientBuilder }
 
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 import scala.collection.immutable.Seq
-import scala.concurrent.{ ExecutionContext, Future }
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.{ ExecutionContext, Future }
 
 object Ec2TagBasedSimpleServiceDiscovery {
 
@@ -33,20 +33,46 @@ object Ec2TagBasedSimpleServiceDiscovery {
         assert(kv.length == 2, "failed to parse one of the key-value pairs in filters")
         new Filter(kv(0), List(kv(1)).asJava)
       })
+
+  @tailrec
+  private[ec2] def getInstances(client: AmazonEC2,
+                                filters: List[Filter],
+                                nextToken: Option[String],
+                                accumulator: List[String] = List()): List[String] = {
+
+    val describeInstancesRequest = new DescribeInstancesRequest().withFilters(filters.asJava) // withFilters is a set operation
+
+    val describeInstancesResult = client.describeInstances(describeInstancesRequest).withNextToken(nextToken.orNull)
+
+    val ips: List[String] = describeInstancesResult.getReservations.asScala.toList
+      .flatMap((r: Reservation) => r.getInstances.asScala.toList)
+      .map(instance => instance.getPrivateIpAddress)
+
+    val accumulatedIps = accumulator ++ ips
+
+    Option(describeInstancesResult.getNextToken) match {
+      case None =>
+        accumulatedIps
+      case nextToken @ Some(_) =>
+        getInstances(client, filters, nextToken, accumulatedIps)
+    }
+
+  }
+
 }
 
 class Ec2TagBasedSimpleServiceDiscovery(system: ActorSystem) extends SimpleServiceDiscovery {
 
-  private val config = system.settings.config.getConfig("akka.discovery.aws-api-ec2-tag-based")
-
-  private[this] implicit val ec: ExecutionContext = system.dispatcher
-
-  private lazy val ec2Client = {
+  private lazy val ec2Client: AmazonEC2 = {
     val clientConfiguration = new ClientConfiguration()
     // we have our own retry/back-off mechanism, so we don't need EC2Client's in addition
     clientConfiguration.setRetryPolicy(PredefinedRetryPolicies.NO_RETRY_POLICY)
     AmazonEC2ClientBuilder.standard().withClientConfiguration(clientConfiguration).build()
   }
+
+  private[this] implicit val ec: ExecutionContext = system.dispatcher
+
+  private val config = system.settings.config.getConfig("akka.discovery.aws-api-ec2-tag-based")
 
   private val tagKey = config.getString("tag-key")
 
@@ -54,30 +80,6 @@ class Ec2TagBasedSimpleServiceDiscovery(system: ActorSystem) extends SimpleServi
   private val otherFilters = parseFiltersString(otherFiltersString)
 
   private val runningInstancesFilter = new Filter("instance-state-name", List("running").asJava)
-
-  def lookup(name: String): Future[Resolved] = {
-
-    val tagFilter = new Filter("tag:" + tagKey, List(name).asJava)
-
-    val allFilters: util.List[Filter] = (runningInstancesFilter :: tagFilter :: otherFilters).asJava
-
-    val request = new DescribeInstancesRequest().withFilters(allFilters) // withFilters is a set operation
-
-    // pretty quick call on EC2, not worried about blocking
-    Future {
-      // TODO: handle pagination (in rare case you get hundreds of instances, the results of this call come
-      // paginated and the user of the API has to call describeInstances again, with the nextToken
-      ec2Client
-        .describeInstances(request)
-        .getReservations
-        .asScala
-        .toList
-        .flatMap((r: Reservation) => r.getInstances.asScala.toList)
-        .map(instance => instance.getPrivateIpAddress)
-        .map((ip: String) => ResolvedTarget(ip, None))
-    }.map(Resolved(name, _))
-
-  }
 
   override def lookup(name: String, resolveTimeout: FiniteDuration): Future[Resolved] =
     Future.firstCompletedOf(
@@ -88,5 +90,17 @@ class Ec2TagBasedSimpleServiceDiscovery(system: ActorSystem) extends SimpleServi
         lookup(name)
       )
     )
+
+  def lookup(name: String): Future[Resolved] = {
+
+    val tagFilter = new Filter("tag:" + tagKey, List(name).asJava)
+
+    val allFilters: List[Filter] = runningInstancesFilter :: tagFilter :: otherFilters
+
+    Future {
+      getInstances(ec2Client, allFilters, None).map((ip: String) => ResolvedTarget(ip, None))
+    }.map(resoledTargets => Resolved(name, resoledTargets))
+
+  }
 
 }
