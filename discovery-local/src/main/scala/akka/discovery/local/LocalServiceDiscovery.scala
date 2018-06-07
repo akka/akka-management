87c81158342c17dfcc296a193b863f7b084e84d2
herzrasen <dennis.mellert@gmail.com>
2018-06-07 06:49:08 +0200

    Service Discovery implementation using the service file

--- discovery-local/src/main/scala/akka/discovery/local/LocalServiceDiscovery.scala
+++ discovery-local/src/main/scala/akka/discovery/local/LocalServiceDiscovery.scala
@@ -1,5 +1,60 @@
+/*
+ * Copyright (C) 2017 Lightbend Inc. <http://www.lightbend.com>
+ */
 package akka.discovery.local
 
-class LocalServiceDiscovery {
+import java.nio.file.Paths
 
+import akka.actor.ActorSystem
+import akka.discovery.SimpleServiceDiscovery
+import akka.discovery.local.registration.LocalServiceRegistration
+import akka.event.Logging
+
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
+
+/**
+ * Looks for an entry in the service file
+ */
+class LocalServiceDiscovery(system: ActorSystem) extends SimpleServiceDiscovery {
+  import SimpleServiceDiscovery._
+
+  private val log = Logging(system, getClass)
+  private val serviceFile =
+    system.settings.config.getString("akka.discovery.akka-local.service-file")
+  private val hostname =
+    system.settings.config.getString("akka.remote.netty.tcp.hostname")
+  private val port = system.settings.config.getInt("akka.management.http.port")
+  private val localServiceRegistration = new LocalServiceRegistration(Paths.get(serviceFile))
+
+  override def lookup(name: String, resolveTimeout: FiniteDuration): Future[Resolved] = {
+    registerSelf()
+
+    log.info(s"Using $serviceFile as service file")
+    val addresses =
+      localServiceRegistration.localServiceEntries.map { entry =>
+        ResolvedTarget(entry.addr, Some(entry.port))
+      }
+
+    sys.addShutdownHook {
+      unregisterSelf()
+    }
+
+    log.info(s"Discovered $addresses")
+    Future.successful(Resolved(name, addresses))
+  }
+
+  private def registerSelf(): Unit = {
+    log.info("Checking if self is registered in service file")
+
+    if (!localServiceRegistration.isRegistered(hostname, port)) {
+      log.info(s"Registering $hostname:$port")
+      localServiceRegistration.add(hostname, port)
+    }
+  }
+
+  private def unregisterSelf(): Unit = {
+    log.info(s"Unregistering $hostname:$port from $serviceFile")
+    localServiceRegistration.remove(hostname, port)
+  }
 }
