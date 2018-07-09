f6591f5174fd3d132cc33376110ce68880709c36
Patrik Nordwall <patrik.nordwall@gmail.com>
2018-07-09 16:19:58 +0200

    make ServiceDiscovery.loadServiceDiscovery public (#230)
    * make ServiceDiscovery.loadServiceDiscovery public

--- discovery/src/main/scala/akka/discovery/ServiceDiscovery.scala
+++ discovery/src/main/scala/akka/discovery/ServiceDiscovery.scala
@@ -3,12 +3,20 @@
  */
 package akka.discovery
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.function.{ Function => JFunction }
+
 import akka.actor._
 import akka.annotation.InternalApi
 
 final class ServiceDiscovery(implicit system: ExtendedActorSystem) extends Extension {
 
-  private lazy val _simpleImplMethod =
+  private val implementations = new ConcurrentHashMap[String, SimpleServiceDiscovery]
+  private val factory = new JFunction[String, SimpleServiceDiscovery] {
+    override def apply(method: String): SimpleServiceDiscovery = createServiceDiscovery(method)
+  }
+
+  private lazy val _defaultImplMethod =
     system.settings.config.getString("akka.discovery.method") match {
       case "<method>" ⇒
         throw new IllegalArgumentException(
@@ -18,7 +26,7 @@ final class ServiceDiscovery(implicit system: ExtendedActorSystem) extends Exten
       case method ⇒ method
     }
 
-  private lazy val _simpleImpl = ServiceDiscovery.loadServiceDiscovery(_simpleImplMethod, system)
+  private lazy val _simpleImpl = loadServiceDiscovery(_defaultImplMethod)
 
   /**
    * Default [[SimpleServiceDiscovery]] as configured in `akka.discovery.method`.
@@ -26,22 +34,20 @@ final class ServiceDiscovery(implicit system: ExtendedActorSystem) extends Exten
   @throws[IllegalArgumentException]
   def discovery: SimpleServiceDiscovery = _simpleImpl
 
-}
-
-object ServiceDiscovery extends ExtensionId[ServiceDiscovery] with ExtensionIdProvider {
-  override def apply(system: ActorSystem): ServiceDiscovery = super.apply(system)
-
-  override def lookup: ServiceDiscovery.type = ServiceDiscovery
-
-  override def get(system: ActorSystem): ServiceDiscovery = super.get(system)
-
-  override def createExtension(system: ExtendedActorSystem): ServiceDiscovery = new ServiceDiscovery()(system)
-
   /**
-   * INTERNAL API
+   * Create a [[SimpleServiceDiscovery]] from configuration property.
+   * The given `method` parameter is used to find configuration property
+   * "akka.discovery.[method].class" or "[method].class". `method` can also
+   * be a fully class name.
+   *
+   * The `SimpleServiceDiscovery` instance for a given `method` will be created
+   * once and subsequent requests for the same `method` will return the same instance.
    */
-  @InternalApi
-  private[akka] def loadServiceDiscovery(method: String, system: ExtendedActorSystem): SimpleServiceDiscovery = {
+  def loadServiceDiscovery(method: String): SimpleServiceDiscovery = {
+    implementations.computeIfAbsent(method, factory)
+  }
+
+  private def createServiceDiscovery(method: String): SimpleServiceDiscovery = {
     val config = system.settings.config
     val dynamic = system.dynamicAccess
 
@@ -62,18 +68,30 @@ object ServiceDiscovery extends ExtensionId[ServiceDiscovery] with ExtensionIdPr
         }
     }
 
-    val i = create(classNameFromConfig("akka.discovery." + method + ".class")).recoverWith {
+    val configName = "akka.discovery." + method + ".class"
+    val instance = create(classNameFromConfig(configName)).recoverWith {
       case _ ⇒ create(classNameFromConfig(method + ".class"))
     }.recoverWith {
       case _ ⇒ create(method) // so perhaps, it is a classname?
     }
 
-    i.getOrElse(
+    instance.getOrElse(
       throw new IllegalArgumentException(
-          s"Illegal `akka.discovery.method` value '$method' or incompatible class! " +
+          s"Illegal [$configName] value or incompatible class! " +
           "The implementation class MUST extend akka.discovery.SimpleServiceDiscovery and take an " +
-          "ExtendedActorSystem as constructor argument.", i.failed.get)
+          "ExtendedActorSystem as constructor argument.", instance.failed.get)
     )
   }
+
+}
+
+object ServiceDiscovery extends ExtensionId[ServiceDiscovery] with ExtensionIdProvider {
+  override def apply(system: ActorSystem): ServiceDiscovery = super.apply(system)
+
+  override def lookup: ServiceDiscovery.type = ServiceDiscovery
+
+  override def get(system: ActorSystem): ServiceDiscovery = super.get(system)
+
+  override def createExtension(system: ExtendedActorSystem): ServiceDiscovery = new ServiceDiscovery()(system)
 
 }
--- discovery/src/test/scala/akka/discovery/DiscoveryConfigurationSpec.scala
+++ discovery/src/test/scala/akka/discovery/DiscoveryConfigurationSpec.scala
@@ -3,40 +3,43 @@
  */
 package akka.discovery
 
-import akka.actor.{ ActorSystem, ExtendedActorSystem }
-import com.typesafe.config.ConfigFactory
-import org.scalatest.{ Matchers, WordSpec }
-
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import com.typesafe.config.ConfigFactory
+import org.scalatest.Matchers
+import org.scalatest.WordSpec
 
 class DiscoveryConfigurationSpec extends WordSpec with Matchers {
 
   "ServiceDiscovery" should {
     "throw when no default discovery configured" in {
-      val sys = ActorSystem()
-
-      val ex = intercept[Exception] {
-        ServiceDiscovery(sys).discovery
-      }
-      ex.getMessage should include("No default service discovery implementation configured")
+      val sys = ActorSystem("DiscoveryConfigurationSpec")
+      try {
+        val ex = intercept[Exception] {
+          ServiceDiscovery(sys).discovery
+        }
+        ex.getMessage should include("No default service discovery implementation configured")
+      } finally TestKit.shutdownActorSystem(sys)
     }
 
     "select implementation from config by classname" in {
       val className = classOf[FakeTestDiscovery].getCanonicalName
 
-      val sys = ActorSystem("example", ConfigFactory.parseString(s"""
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
           akka.discovery.method = $className
-        """.stripMargin).withFallback(ConfigFactory.load()))
+        """).withFallback(ConfigFactory.load()))
 
       try ServiceDiscovery(sys).discovery.getClass.getCanonicalName should ===(className)
-      finally sys.terminate()
+      finally TestKit.shutdownActorSystem(sys)
     }
 
     "select implementation from config by config name (inside akka.discovery namespace)" in {
       val className = classOf[FakeTestDiscovery].getCanonicalName
 
-      val sys = ActorSystem("example", ConfigFactory.parseString(s"""
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
             akka.discovery {
               method = akka-mock-inside
 
@@ -44,27 +47,88 @@ class DiscoveryConfigurationSpec extends WordSpec with Matchers {
                 class = $className
               }
             }
-        """.stripMargin).withFallback(ConfigFactory.load()))
+        """).withFallback(ConfigFactory.load()))
 
       try ServiceDiscovery(sys).discovery.getClass.getCanonicalName should ===(className)
-      finally sys.terminate()
+      finally TestKit.shutdownActorSystem(sys)
     }
 
     "select implementation from config by config name (outside akka.discovery namespace)" in {
       val className = classOf[FakeTestDiscovery].getCanonicalName
 
-      val sys = ActorSystem("example", ConfigFactory.parseString(s"""
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
             akka.discovery {
-              method = com.akka-mock-inside
+              method = com.akka-mock-outside
             }
 
-            com.akka-mock-inside {
+            com.akka-mock-outside {
               class = $className
             }
-        """.stripMargin).withFallback(ConfigFactory.load()))
+        """).withFallback(ConfigFactory.load()))
 
       try ServiceDiscovery(sys).discovery.getClass.getCanonicalName should ===(className)
-      finally sys.terminate()
+      finally TestKit.shutdownActorSystem(sys)
+    }
+
+    "select implementation from full class name" in {
+      val className = classOf[FakeTestDiscovery].getCanonicalName
+
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
+            akka.discovery {
+              method = "$className"
+            }
+        """).withFallback(ConfigFactory.load()))
+
+      try ServiceDiscovery(sys).discovery.getClass.getCanonicalName should ===(className)
+      finally TestKit.shutdownActorSystem(sys)
+    }
+
+    "load another implementation from config by config name" in {
+      val className1 = classOf[FakeTestDiscovery].getCanonicalName
+      val className2 = classOf[FakeTestDiscovery2].getCanonicalName
+
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
+            akka.discovery {
+              method = mock1
+
+              mock1 {
+                class = $className1
+              }
+              mock2 {
+                class = $className2
+              }
+            }
+        """).withFallback(ConfigFactory.load()))
+
+      try {
+        ServiceDiscovery(sys).discovery.getClass.getCanonicalName should ===(className1)
+        ServiceDiscovery(sys).loadServiceDiscovery("mock2").getClass.getCanonicalName should ===(className2)
+      } finally TestKit.shutdownActorSystem(sys)
+    }
+
+    "return same instance for same method" in {
+      val className1 = classOf[FakeTestDiscovery].getCanonicalName
+      val className2 = classOf[FakeTestDiscovery2].getCanonicalName
+
+      val sys = ActorSystem("DiscoveryConfigurationSpec", ConfigFactory.parseString(s"""
+            akka.discovery {
+              method = mock1
+
+              mock1 {
+                class = $className1
+              }
+              mock2 {
+                class = $className2
+              }
+            }
+        """).withFallback(ConfigFactory.load()))
+
+      try {
+        ServiceDiscovery(sys).loadServiceDiscovery("mock2") should be theSameInstanceAs ServiceDiscovery(sys)
+          .loadServiceDiscovery("mock2")
+
+        ServiceDiscovery(sys).discovery should be theSameInstanceAs ServiceDiscovery(sys).loadServiceDiscovery("mock1")
+      } finally TestKit.shutdownActorSystem(sys)
     }
   }
 
@@ -74,3 +138,5 @@ class FakeTestDiscovery extends SimpleServiceDiscovery {
   override def lookup(name: String, resolveTimeout: FiniteDuration): Future[SimpleServiceDiscovery.Resolved] =
     ???
 }
+
+class FakeTestDiscovery2 extends FakeTestDiscovery
