a4d31a25850fd86e004ea86520b7fd76ad429916
Arnout Engelen <arnouten@bzzt.net>
2018-07-11 17:58:56 +0200

    Seq->Set

--- cluster-bootstrap/src/main/scala/akka/management/cluster/bootstrap/ClusterBootstrap.scala
+++ cluster-bootstrap/src/main/scala/akka/management/cluster/bootstrap/ClusterBootstrap.scala
@@ -8,7 +8,6 @@ import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.concurrent.Promise
-import scala.collection.immutable
 
 import akka.actor.ActorSystem
 import akka.actor.ExtendedActorSystem
@@ -109,17 +108,17 @@ final class ClusterBootstrap(implicit system: ExtendedActorSystem) extends Exten
     _selfContactPointUri.success(baseUri)
 
   /** INTERNAL API */
-  @InternalApi private[akka] def selfContactPoints: Future[immutable.Seq[(String, Int)]] =
+  @InternalApi private[akka] def selfContactPoints: Future[Set[(String, Int)]] =
     _selfContactPointUri.future.map { uri =>
       if (system.settings.config.hasPath("akka.discovery.kubernetes-api.pod-domain") && uri.authority.host.isIPv4()) {
         val podNamespace = system.settings.config.getString("akka.discovery.kubernetes-api.pod-namespace")
         val podDomain = system.settings.config.getString("akka.discovery.kubernetes-api.pod-domain")
-        immutable.Seq(
+        Set(
           (uri.authority.host.toString, uri.authority.port),
           (s"${uri.authority.host.toString.replace('.', '-')}.${podNamespace}.pod.${podDomain}", uri.authority.port)
         )
       } else {
-        immutable.Seq((uri.authority.host.toString, uri.authority.port))
+        Set((uri.authority.host.toString, uri.authority.port))
       }
     }
 
