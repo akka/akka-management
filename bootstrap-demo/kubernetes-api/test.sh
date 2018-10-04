e288d53bf68abb1cd86dd81dbe935bc747b2365d
Arnout Engelen <arnouten@bzzt.net>
2018-10-04 12:13:38 +0200

    Nicer retry loop

--- bootstrap-demo/kubernetes-api/test.sh
+++ bootstrap-demo/kubernetes-api/test.sh
@@ -6,18 +6,15 @@ sbt bootstrap-demo-kubernetes-api/docker:publishLocal
 
 kubectl apply -f bootstrap-demo/kubernetes-api/kubernetes/akka-cluster.yml
 
-TRY=0
-
-until [ $TRY -ge 10 ]
+for i in {1..10}
 do
   echo "Waiting for pods to get ready..."
   kubectl get pods
   [ `kubectl get pods | grep Running | wc -l` -eq 2 ] && break
   sleep 4
-  TRY=$[$TRY+1]
 done
 
-if [ $TRY -eq 10 ]
+if [ $i -eq 10 ]
 then
   echo "Pods did not get ready"
   exit -1
@@ -25,18 +22,15 @@ fi
 
 POD=$(kubectl get pods | grep appka | grep Running | head -n1 | awk '{ print $1 }')
 
-TRY=0
-
-until [ $TRY -ge 10 ]
+for i in {1..10}
 do
   echo "Checking for MemberUp logging..."
   kubectl logs $POD | grep MemberUp || true
   [ `kubectl logs $POD | grep MemberUp | wc -l` -eq 2 ] && break
   sleep 3
-  TRY=$[$TRY+1]
 done
 
-if [ $TRY -eq 10 ]
+if [ $i -eq 10 ]
 then
   echo "No 2 MemberUp log events found"
   exit -1
