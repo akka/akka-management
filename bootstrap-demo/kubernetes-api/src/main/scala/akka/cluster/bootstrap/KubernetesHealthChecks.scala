178828c284f9aeabacef2ea3305d5cbecca99887
Arnout Engelen <arnouten@bzzt.net>
2018-10-25 05:06:41 +0200

    Show readiness probe for k8s-api-scala demo

---
+++ bootstrap-demo/kubernetes-api/src/main/scala/akka/cluster/bootstrap/KubernetesHealthChecks.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2017 Lightbend Inc. <http://www.lightbend.com>
+ */
+package akka.cluster.bootstrap
+
+import akka.actor.ActorSystem
+import akka.cluster.{ Cluster, MemberStatus }
+import akka.event.{ Logging, LoggingAdapter }
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+class KubernetesHealthChecks(system: ActorSystem) {
+
+  val log: LoggingAdapter = Logging(system, getClass)
+  val cluster = Cluster(system)
+
+  //#health
+  private val readyStates: Set[MemberStatus] = Set(MemberStatus.Up, MemberStatus.WeaklyUp)
+
+  val k8sHealthChecks: Route =
+    concat(
+        path("ready") {
+          get {
+            val selfState = cluster.selfMember.status
+            log.debug("ready? clusterState {}", selfState)
+            if (readyStates.contains(selfState)) complete(StatusCodes.OK)
+            else complete(StatusCodes.InternalServerError)
+          }
+        },
+        path("alive") {
+          get {
+            // When Akka HTTP can respond to requests, that is sufficient
+            // to consider ourselves 'live': we don't want K8s to kill us even
+            // when we're in the process of shutting down (only stop sending
+            // us traffic, which is done due to the readyness check then failing)
+            complete(StatusCodes.OK)
+          }
+        })
+  //#health
+}
