57b417ebbad5f1929159615f41686a4d98aa387d
Christopher Batey <christopher.batey@gmail.com>
2018-11-01 12:35:26 +0000

    Really fix test

--- bootstrap-demo/kubernetes-api/test.sh
+++ bootstrap-demo/kubernetes-api/test.sh
@@ -11,7 +11,7 @@ for i in {1..10}
 do
   echo "Waiting for pods to get ready..."
   kubectl get pods --namespace akka-bootstrap
-  [ `kubectl get pods --namespace akka-bootstrap | grep Running | wc -l` -eq 2 ] && break
+  [ `kubectl get pods --namespace akka-bootstrap | grep Running | wc -l` -eq 3 ] && break
   sleep 4
 done
 
@@ -21,19 +21,19 @@ then
   exit -1
 fi
 
-POD=$(kubectl get pods --namespace akka-bootstrap | grep appka | grep Running | head -n1 | awk '{ print $1 }')
+POD=$(kubectl get pods --namespace akka-bootstrap | grep akka-bootstrap-demo | grep Running | head -n1 | awk '{ print $1 }')
 
 for i in {1..10}
 do
   echo "Checking for MemberUp logging..."
   kubectl logs $POD --namespace akka-bootstrap | grep MemberUp || true
-  [ `kubectl logs --namespace akka-bootstrap $POD | grep MemberUp | wc -l` -eq 2 ] && break
+  [ `kubectl logs --namespace akka-bootstrap $POD | grep MemberUp | wc -l` -eq 3 ] && break
   sleep 3
 done
 
 if [ $i -eq 10 ]
 then
-  echo "No 2 MemberUp log events found"
+  echo "No 3 MemberUp log events found"
   echo "=============================="
   kubectl logs $POD
   exit -1
