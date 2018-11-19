578ccbbb916281f21f3f4853c62ab5f392ad9f53
Christopher Batey <christopher.batey@gmail.com>
2018-11-19 14:43:21 +0000

    Keep compiler happy

--- cluster-http/src/main/scala/akka/management/cluster/ClusterHttpManagementRoutes.scala
+++ cluster-http/src/main/scala/akka/management/cluster/ClusterHttpManagementRoutes.scala
@@ -89,7 +89,7 @@ object ClusterHttpManagementRoutes extends ClusterHttpManagementJsonProtocol {
         m ⇒ s"${m.uniqueAddress.address}" == memberAddress || m.uniqueAddress.address.hostPort == memberAddress)
   }
 
-  private def routeGetMember(cluster: Cluster) =
+  private def routeFindMember(cluster: Cluster) =
     path(Remaining) { memberAddress ⇒
       findMember(cluster, memberAddress) match {
         case Some(member) ⇒
@@ -134,7 +134,7 @@ object ClusterHttpManagementRoutes extends ClusterHttpManagementJsonProtocol {
           pathEndOrSingleSlash {
             routeGetMembers(cluster) ~ routePostMembers(cluster)
           },
-          routeGetMember(cluster)
+          routeFindMember(cluster)
         )
       },
       pathPrefix("cluster" / "shards" / Remaining) { shardRegionName =>
@@ -152,7 +152,7 @@ object ClusterHttpManagementRoutes extends ClusterHttpManagementJsonProtocol {
           pathEndOrSingleSlash {
             routeGetMembers(cluster)
           },
-          routeGetMember(cluster)
+          routeFindMember(cluster)
         )
       },
       pathPrefix("cluster" / "shards" / Remaining) { shardRegionName =>
