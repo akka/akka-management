cb92fd88d2e636e43ae884fe95a81af7e8567315
Johannes Rudolph <johannes.rudolph@gmail.com>
2018-11-21 11:59:15 +0100

    Pull jackson-databind to the latest and more secure version 2.9.7 when it's pulled in transitively
    It seems there have already been more vulnerabilities found that will be fixed
    in 2.9.8. See https://github.com/FasterXML/jackson-databind/blob/master/release-notes/VERSION-2.x#L25https://github.com/FasterXML/jackson-databind/blob/081ef5b62fe1c79aebadde2f75c99bc26fa3e140/release-notes/VERSION-2.x#L25

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,6 +53,27 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-http-testkit"                  % AkkaHttpVersion % "test"
     )
 
+    // often called-in transitively with insecure versions of databind / core
+    val JacksonDatabind = Seq(
+      "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7"
+    )
+
+    val ConsulClient = Seq(
+      //License: Apache 2.0
+      "com.orbitz.consul" % "consul-client" % "1.1.2",
+      //License: Apache 2.0
+      "com.pszymczyk.consul" % "embedded-consul" % "1.0.2" % "test"
+    ) ++ JacksonDatabind // consul depends on insecure version of jackson
+
+    val AwsJavaSdkEc2Ecs = Seq(
+      "com.amazonaws" % "aws-java-sdk-ec2" % "1.11.292",
+      "com.amazonaws" % "aws-java-sdk-ecs" % "1.11.292"
+    ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
+
+    val Aws2Ecs = Seq(
+      "software.amazon.awssdk" % "ecs" % "2.0.0-preview-9"
+    ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
+
     // For demos
     val Logging = Seq(
       "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
@@ -88,12 +109,7 @@ object Dependencies {
     libraryDependencies ++=
       DependencyGroups.AkkaActor ++
       DependencyGroups.AkkaTesting ++
-      Seq(
-        //License: Apache 2.0
-        "com.orbitz.consul" % "consul-client" % "1.1.2",
-        //License: Apache 2.0
-        "com.pszymczyk.consul" % "embedded-consul" % "1.0.2" % "test"
-      )
+      DependencyGroups.ConsulClient
   )
 
   val DiscoveryKubernetesApi = Seq(
@@ -110,16 +126,16 @@ object Dependencies {
 
   val DiscoveryAwsApi = Seq(
     libraryDependencies ++=
-      DependencyGroups.AkkaActor ++ Seq(
-        "com.amazonaws" % "aws-java-sdk-ec2" % "1.11.292",
-        "com.amazonaws" % "aws-java-sdk-ecs" % "1.11.292")
+      DependencyGroups.AkkaActor ++
+      DependencyGroups.AwsJavaSdkEc2Ecs // aws depends on insecure version
   )
 
   val DiscoveryAwsApiAsync = Seq(
     libraryDependencies ++=
-      DependencyGroups.AkkaActor ++ Seq(
-        "software.amazon.awssdk" % "ecs" % "2.0.0-preview-9",
-        "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0")
+      DependencyGroups.AkkaActor ++
+      DependencyGroups.Aws2Ecs ++ Seq(
+        "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0"
+      )
   )
 
   val ManagementHttp = Seq(
