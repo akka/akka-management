b8b61d9c9fd0e88d208d626afaad8d4531ed0823
Sean Sullivan <github@seansullivan.com>
2019-01-16 17:02:13 -0800

    AWS SDK 2.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -78,7 +78,7 @@ object Dependencies {
     ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
     val Aws2Ecs = Seq(
-      "software.amazon.awssdk" % "ecs" % "2.2.0"
+      "software.amazon.awssdk" % "ecs" % "2.3.3"
     ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
     // For demos
