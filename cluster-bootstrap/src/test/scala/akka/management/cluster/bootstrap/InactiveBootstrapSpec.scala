b89bf4209b62ab61524d1cc15b3304c042db5853
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-12-05 10:56:26 +0100

    bootstrap: access discovery lazily in ClusterBootstrap extension
    This allows to have the cluster-bootstrap module on the classpath without
    actually configuring it. The management routes automatically instantiate
    ClusterBootstrap when on the classpath. This fails when discovery is not
    configured.
    
    Fixes #519.

---
+++ cluster-bootstrap/src/test/scala/akka/management/cluster/bootstrap/InactiveBootstrapSpec.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2017-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.management.cluster.bootstrap
+
+import akka.actor.ActorSystem
+import akka.management.scaladsl.AkkaManagement
+
+class InactiveBootstrapSpec extends AbstractBootstrapSpec {
+  val system = ActorSystem("InactiveBootstrapSpec")
+
+  "cluster-bootstrap on the classpath" should {
+    "not fail management routes if bootstrap is not configured or used" in {
+      // this will call ClusterBootstrap(system) which should not fail even if discovery is not configured
+      AkkaManagement(system)
+    }
+  }
+
+  override protected def afterAll(): Unit = system.terminate()
+}
