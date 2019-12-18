0d2d7667f519b0b644239beb405fefcfd81fb754
Milan <milanvdm@hotmail.com>
2019-12-18 17:16:29 +0100

    Run ScalaFmt

--- discovery-aws-api-async/src/main/scala/akka/discovery/awsapi/ecs/AsyncEcsServiceDiscovery.scala
+++ discovery-aws-api-async/src/main/scala/akka/discovery/awsapi/ecs/AsyncEcsServiceDiscovery.scala
@@ -4,26 +4,26 @@
 
 package akka.discovery.awsapi.ecs
 
-import java.net.{InetAddress, NetworkInterface}
+import java.net.{ InetAddress, NetworkInterface }
 import java.util.concurrent.TimeoutException
 
 import scala.collection.JavaConverters._
 import scala.collection.immutable.Seq
 import scala.compat.java8.FutureConverters.toScala
 import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 
 import akka.actor.ActorSystem
 import akka.annotation.ApiMayChange
-import akka.discovery.ServiceDiscovery.{Resolved, ResolvedTarget}
+import akka.discovery.ServiceDiscovery.{ Resolved, ResolvedTarget }
 import akka.discovery.awsapi.ecs.AsyncEcsServiceDiscovery._
-import akka.discovery.{Lookup, ServiceDiscovery}
+import akka.discovery.{ Lookup, ServiceDiscovery }
 import akka.pattern.after
 import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration
 import software.amazon.awssdk.core.retry.RetryPolicy
 import software.amazon.awssdk.services.ecs._
-import software.amazon.awssdk.services.ecs.model.{Tag => _, _}
+import software.amazon.awssdk.services.ecs.model.{ Tag => _, _ }
 
 @ApiMayChange
 class AsyncEcsServiceDiscovery(system: ActorSystem) extends ServiceDiscovery {
@@ -100,7 +100,8 @@ object AsyncEcsServiceDiscovery {
     }
 
   private def resolveTasks(ecsClient: EcsAsyncClient, cluster: String, serviceName: String, tags: List[Tag])(
-    implicit ec: ExecutionContext): Future[Seq[Task]] =
+      implicit ec: ExecutionContext
+  ): Future[Seq[Task]] =
     for {
       taskArns <- listTaskArns(ecsClient, cluster, serviceName)
       tasks <- describeTasks(ecsClient, cluster, taskArns)
@@ -115,7 +116,8 @@ object AsyncEcsServiceDiscovery {
       cluster: String,
       serviceName: String,
       pageTaken: Option[String] = None,
-      accumulator: Seq[String] = Seq.empty)(implicit ec: ExecutionContext): Future[Seq[String]] =
+      accumulator: Seq[String] = Seq.empty
+  )(implicit ec: ExecutionContext): Future[Seq[String]] =
     for {
       listTasksResponse <- toScala(
         ecsClient.listTasks(
@@ -145,7 +147,8 @@ object AsyncEcsServiceDiscovery {
     } yield taskArns
 
   private[this] def describeTasks(ecsClient: EcsAsyncClient, cluster: String, taskArns: Seq[String])(
-    implicit ec: ExecutionContext): Future[Seq[Task]] =
+      implicit ec: ExecutionContext
+  ): Future[Seq[Task]] =
     for {
       // Each DescribeTasksRequest can contain at most 100 task ARNs.
       describeTasksResponses <- Future.traverse(taskArns.grouped(100))(
