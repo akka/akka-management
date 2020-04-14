b97cd5b025815c7f0b2fb6ae905a222795b9a534
Arnout Engelen <arnout@bzzt.net>
2020-04-14 12:02:50 +0200

    Update copyright to 2020

--- project/Common.scala
+++ project/Common.scala
@@ -27,7 +27,7 @@ object Common extends AutoPlugin {
         ),
       licenses := Seq(("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0"))),
       description := "Akka Management is a suite of tools for operating Akka Clusters.",
-      headerLicense := Some(HeaderLicense.Custom("Copyright (C) 2017-2019 Lightbend Inc. <https://www.lightbend.com>")),
+      headerLicense := Some(HeaderLicense.Custom("Copyright (C) 2017-2020 Lightbend Inc. <https://www.lightbend.com>")),
       crossScalaVersions := Seq(Dependencies.Scala211, Dependencies.Scala212, Dependencies.Scala213),
       projectInfoVersion := (if (isSnapshot.value) "snapshot" else version.value),
       crossVersion := CrossVersion.binary,
