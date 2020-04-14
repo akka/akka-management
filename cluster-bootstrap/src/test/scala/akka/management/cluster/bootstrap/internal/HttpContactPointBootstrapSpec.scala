779ce5fa4b52b07a29100d6aa5bac725be90c031
Arnout Engelen <arnout@bzzt.net>
2020-04-14 12:00:06 +0200

    Avoid invalid characters in HttpContactPointBootstrap name #657

---
+++ cluster-bootstrap/src/test/scala/akka/management/cluster/bootstrap/internal/HttpContactPointBootstrapSpec.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017-2019 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.management.cluster.bootstrap.internal
+
+import akka.actor.ActorPath
+import akka.http.scaladsl.model.Uri.Host
+
+import org.scalatest.WordSpec
+import org.scalatest.Matchers
+
+class HttpContactPointBootstrapSpec extends WordSpec with Matchers {
+    "HttpContactPointBootstrap" should {
+        "use a safe name when connecting over IPv6" in {
+            val name = HttpContactPointBootstrap.name(Host("[fe80::1013:2070:258a:c662]"), 443)
+            ActorPath.isValidPathElement(name) should be(true)
+        }
+    }
+}
