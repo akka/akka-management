66ca4ebce732b33cc158f1513f7608517ddfe7d5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-10 11:54:08 +0200

    ecs 2.13.71 (was 2.13.53) (#741)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -107,7 +107,7 @@ object Dependencies {
       ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
     val Aws2Ecs = Seq(
-        "software.amazon.awssdk" % "ecs" % "2.13.53"
+        "software.amazon.awssdk" % "ecs" % "2.13.71"
       ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
     val Logging = Seq(
