9d100881665d024304e61d32bcd8a55905450456
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-10 11:43:01 +0200

    aws-java-sdk-ec2, aws-java-sdk-ecs 1.11.837 (was 1.11.761) (#734)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
 
   val SprayJsonVersion = "1.3.5"
 
-  val AwsSdkVersion = "1.11.761"
+  val AwsSdkVersion = "1.11.837"
   val JacksonDatabindVersion = "2.10.4"
 
   object TestDeps {
