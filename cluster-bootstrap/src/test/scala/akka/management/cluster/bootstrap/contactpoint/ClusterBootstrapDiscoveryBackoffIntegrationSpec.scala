afeaa4b396ad3cd36aead47403b22186b67e01ae
Johan Andrén <johan@markatta.com>
2020-10-20 15:10:07 +0200

    Keep timing separate for the two systems to avoid races (#775)

--- cluster-bootstrap/src/test/scala/akka/management/cluster/bootstrap/contactpoint/ClusterBootstrapDiscoveryBackoffIntegrationSpec.scala
+++ cluster-bootstrap/src/test/scala/akka/management/cluster/bootstrap/contactpoint/ClusterBootstrapDiscoveryBackoffIntegrationSpec.scala
@@ -5,6 +5,8 @@
 package akka.management.cluster.bootstrap.contactpoint
 
 import java.net.InetAddress
+import java.util.concurrent.ConcurrentHashMap
+import java.util.function.{ BiConsumer, BiFunction }
 
 import akka.actor.ActorSystem
 import akka.cluster.Cluster
@@ -19,6 +21,7 @@ import akka.testkit.{ SocketUtil, TestKit, TestProbe }
 import com.typesafe.config.{ Config, ConfigFactory }
 import org.scalactic.Tolerance
 import org.scalatest.concurrent.ScalaFutures
+
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import org.scalatest.matchers.should.Matchers
@@ -102,45 +105,51 @@ class ClusterBootstrapDiscoveryBackoffIntegrationSpec
     // times it is called, thus testing that discovery is called multiple times and
     // that formation will eventually succeed once discovery returns reachable addresses
 
-    var called = 0
-    var call2Timestamp = 0L
-    var call3Timestamp = 0L
+    case class SystemStats(called: Int = 0, call2Timestamp: Long = 0, call3Timestamp: Long = 0)
+    val perSystemStats = new ConcurrentHashMap[ActorSystem, SystemStats]()
+    perSystemStats.put(systemA, SystemStats())
+    perSystemStats.put(systemB, SystemStats())
     val name = s"$systemName.svc.cluster.local"
 
     MockDiscovery.set(
-      Lookup(name).withProtocol("tcp").withPortName("management"), { () =>
-        this.synchronized {
-          called += 1
-
-          if (called == 2)
-            call2Timestamp = System.nanoTime()
-          else if (called == 3)
-            call3Timestamp = System.nanoTime()
-
-          val res =
-            if (called < 4)
-              Future.failed(new Exception("Boom! Discovery failed, was rate limited for example..."))
-            else
-              Future.successful(
-                Resolved(
-                  name,
-                  List(
-                    ResolvedTarget(
-                      host = clusterA.selfAddress.host.get,
-                      port = contactPointPorts.get("A"),
-                      address = Option(InetAddress.getByName(clusterA.selfAddress.host.get))
-                    ),
-                    ResolvedTarget(
-                      host = clusterB.selfAddress.host.get,
-                      port = contactPointPorts.get("B"),
-                      address = Option(InetAddress.getByName(clusterB.selfAddress.host.get))
-                    )
+      Lookup(name).withProtocol("tcp").withPortName("management"), { system =>
+        val stats = perSystemStats.compute(
+          system,
+          new BiFunction[ActorSystem, SystemStats, SystemStats] {
+            override def apply(notUsed: ActorSystem, stats: SystemStats): SystemStats = {
+              if (stats.called == 1)
+                stats.copy(called = 2, call2Timestamp = System.nanoTime())
+              else if (stats.called == 2)
+                stats.copy(called = 3, call3Timestamp = System.nanoTime())
+              else
+                stats.copy(called = stats.called + 1)
+            }
+          }
+        )
+        val res =
+          if (stats.called < 4)
+            Future.failed(new Exception("Boom! Discovery failed, was rate limited for example..."))
+          else
+            Future.successful(
+              Resolved(
+                name,
+                List(
+                  ResolvedTarget(
+                    host = clusterA.selfAddress.host.get,
+                    port = contactPointPorts.get("A"),
+                    address = Option(InetAddress.getByName(clusterA.selfAddress.host.get))
+                  ),
+                  ResolvedTarget(
+                    host = clusterB.selfAddress.host.get,
+                    port = contactPointPorts.get("B"),
+                    address = Option(InetAddress.getByName(clusterB.selfAddress.host.get))
                   )
-                ))
+                )
+              ))
 
-          resolveProbe.ref ! DiscoveryRequest(System.currentTimeMillis(), called, res)
-          res
-        }
+        resolveProbe.ref ! DiscoveryRequest(System.currentTimeMillis(), stats.called, res)
+        res
+
       }
     )
 
@@ -161,10 +170,11 @@ class ClusterBootstrapDiscoveryBackoffIntegrationSpec
     }
 
     "poll discovery in exponentially increasing backoffs until eventually joining the returned nodes" in {
-      bootstrapA.discovery.getClass should ===(classOf[MockDiscovery])
+      bootstrapA.discovery shouldBe a[MockDiscovery]
+      bootstrapA.discovery shouldBe a[MockDiscovery]
 
       bootstrapA.start()
-      bootstrapB.start() // deliberately not discovering from this node, however its management port is available
+      bootstrapB.start()
 
       val pA = TestProbe()(systemA)
       clusterA.subscribe(pA.ref, classOf[MemberUp])
@@ -173,12 +183,16 @@ class ClusterBootstrapDiscoveryBackoffIntegrationSpec
       val up1 = pA.expectMsgType[MemberUp](45.seconds)
       info("" + up1)
 
-      this.synchronized {
-        called shouldBe >=(5)
-        val durationBetweenCall2And3 = (call3Timestamp - call2Timestamp).nanos.toMillis
-        info(s"duration between call 2 and 3 ${durationBetweenCall2And3} ms")
-        durationBetweenCall2And3 shouldBe >=(
-          (ClusterBootstrap(systemA).settings.contactPointDiscovery.interval * 2).toMillis)
+      perSystemStats.forEach {
+        new BiConsumer[ActorSystem, SystemStats] {
+          override def accept(system: ActorSystem, stats: SystemStats): Unit = {
+            stats.called shouldBe >=(5)
+            val durationBetweenCall2And3 = (stats.call3Timestamp - stats.call2Timestamp).nanos.toMillis
+            info(s"${Cluster(system).selfAddress}: duration between call 2 and 3 ${durationBetweenCall2And3} ms")
+            durationBetweenCall2And3 shouldBe >=(
+              (ClusterBootstrap(system).settings.contactPointDiscovery.interval * 2).toMillis)
+          }
+        }
       }
     }
 
