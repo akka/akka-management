0fade73864b8f02c8a0053f142cc25a7070a4cb5
Enno Runne <458526+ennru@users.noreply.github.com>
2020-12-21 10:24:17 +0100

    CI: fixed Minikube 1.15.1 version (#827)

--- scripts/setup-minikube-for-linux.sh
+++ scripts/setup-minikube-for-linux.sh
@@ -2,10 +2,10 @@
 
 set -exu
 
-# MINIKUBE_VERSION="latest" -- https://github.com/kubernetes/minikube/issues/2704
-MINIKUBE_VERSION="latest"
+# using Minikube v1.16.0 fails
+MINIKUBE_VERSION="v1.15.1"
 
-# From https://github.com/kubernetes/minikube#linux-continuous-integration-without-vm-support
+# From https://minikube.sigs.k8s.io/docs/tutorials/continuous_integration/
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm kubectl
 
