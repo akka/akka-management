e0a98900204f5744d88f86352a4b1acf2e655db8
Johan Andrén <johan@markatta.com>
2021-02-12 15:09:28 +0100

    Autostart cluster bootstrap if the extension is loaded through config #850

--- cluster-bootstrap/src/main/scala/akka/management/cluster/bootstrap/ClusterBootstrap.scala
+++ cluster-bootstrap/src/main/scala/akka/management/cluster/bootstrap/ClusterBootstrap.scala
@@ -5,11 +5,10 @@
 package akka.management.cluster.bootstrap
 
 import java.util.concurrent.atomic.AtomicReference
-
 import akka.AkkaVersion
-import scala.concurrent.{ Future, Promise, TimeoutException }
-import scala.concurrent.duration._
 
+import scala.concurrent.{Future, Promise, TimeoutException}
+import scala.concurrent.duration._
 import akka.actor.ActorSystem
 import akka.actor.ClassicActorSystemProvider
 import akka.actor.ExtendedActorSystem
@@ -18,12 +17,13 @@ import akka.actor.ExtensionId
 import akka.actor.ExtensionIdProvider
 import akka.annotation.InternalApi
 import akka.cluster.Cluster
-import akka.discovery.{ Discovery, ServiceDiscovery }
+import akka.discovery.{Discovery, ServiceDiscovery}
 import akka.event.Logging
 import akka.http.scaladsl.model.Uri
 import akka.http.scaladsl.server.Route
 import akka.management.cluster.bootstrap.contactpoint.HttpClusterBootstrapRoutes
 import akka.management.cluster.bootstrap.internal.BootstrapCoordinator
+import akka.management.scaladsl.AkkaManagement
 import akka.management.scaladsl.ManagementRouteProviderSettings
 import akka.management.scaladsl.ManagementRouteProvider
 
@@ -63,6 +63,26 @@ final class ClusterBootstrap(implicit system: ExtendedActorSystem) extends Exten
   }
 
   private[this] val _selfContactPointUri: Promise[Uri] = Promise()
+
+
+  private val autostart = system.settings.config.getStringList("akka.extensions")
+    .indexOf(classOf[ClusterBootstrap].getName) > -1
+
+  if (autostart) {
+    log.info("ClusterBootstrap loaded through 'akka.extensions' auto starting management and bootstrap.")
+    // Akka Management hosts the HTTP routes used by bootstrap
+    Future {
+      // we can't let it block extension init
+      try {
+        AkkaManagement(system).start()
+        ClusterBootstrap(system).start()
+      } catch {
+        case ex =>
+          log.error("Failed to autostart cluster bootstrap, terminating system", ex)
+          system.terminate()
+      }
+    }(system.dispatcher)
+  }
 
   override def routes(routeProviderSettings: ManagementRouteProviderSettings): Route = {
     log.info(s"Using self contact point address: ${routeProviderSettings.selfBaseUri}")
---
+++ cluster-bootstrap/src/test/scala/akka/management/cluster/bootstrap/contactpoint/ClusterBootstrapAutostartIntegrationSpec.scala
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2017-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.management.cluster.bootstrap.contactpoint
+
+import akka.actor.ActorSystem
+import akka.cluster.Cluster
+import akka.cluster.MemberStatus
+import akka.discovery.Lookup
+import akka.discovery.MockDiscovery
+import akka.discovery.ServiceDiscovery.Resolved
+import akka.discovery.ServiceDiscovery.ResolvedTarget
+import akka.testkit.SocketUtil
+import akka.testkit.TestKit
+import akka.testkit.TestProbe
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import java.net.InetAddress
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class ClusterBootstrapAutostartIntegrationSpec extends AnyWordSpecLike with Matchers {
+  "Cluster Bootstrap" should {
+
+    var remotingPorts = Map.empty[String, Int]
+    var contactPointPorts = Map.empty[String, Int]
+
+    def config(id: String): Config = {
+      val managementPort = contactPointPorts(id)
+      val remotingPort = remotingPorts(id)
+      info(s"System [$id]: management port: $managementPort")
+      info(s"System [$id]:   remoting port: $remotingPort")
+
+      contactPointPorts = contactPointPorts.updated(id, managementPort)
+      remotingPorts = remotingPorts.updated(id, remotingPort)
+
+      ConfigFactory.parseString(s"""
+        akka {
+          loglevel = INFO
+          # trigger autostart by loading the extension through config
+          extensions = ["akka.management.cluster.bootstrap.ClusterBootstrap"]
+
+          cluster.jmx.multi-mbeans-in-same-jvm = on
+
+          # this can be referred to in tests to use the mock discovery implementation
+          discovery.mock-dns.class = "akka.discovery.MockDiscovery"
+
+          remote.netty.tcp.port = $remotingPort
+          remote.artery.canonical.port = $remotingPort
+          remote.artery.canonical.hostname = "127.0.0.1"
+
+          management {
+            http.port = $managementPort
+            http.hostname = "127.0.0.1"
+            cluster.bootstrap {
+              contact-point-discovery {
+                discovery-method = mock-dns
+
+                service-name = "service"
+                port-name = "management-auto"
+                protocol = "tcp2"
+
+                service-namespace = "svc.cluster.local"
+
+                stable-margin = 4 seconds
+              }
+            }
+          }
+        }
+        """.stripMargin).withFallback(ConfigFactory.load()).resolve()
+    }
+
+    // allocate all ports in one go to avoid clashes
+    val ports = SocketUtil.temporaryServerAddresses(6, "127.0.0.1").map(_.getPort)
+
+    remotingPorts += "A" -> ports(0)
+    remotingPorts += "B" -> ports(1)
+    remotingPorts += "C" -> ports(2)
+
+    contactPointPorts += "A" -> ports(3)
+    contactPointPorts += "B" -> ports(4)
+    contactPointPorts += "C" -> ports(5)
+
+    val systemA = ActorSystem("System", config("A"))
+    val systemB = ActorSystem("System", config("B"))
+    val systemC = ActorSystem("System", config("C"))
+
+    val clusterA = Cluster(systemA)
+    val clusterB = Cluster(systemB)
+    val clusterC = Cluster(systemC)
+
+    // prepare the "mock DNS"
+    val name = "service.svc.cluster.local"
+    MockDiscovery.set(
+      Lookup(name, Some("management-auto"), Some("tcp2")),
+      () =>
+        Future.successful(
+          Resolved(
+            name,
+            List(
+              ResolvedTarget(
+                host = clusterA.selfAddress.host.get,
+                port = contactPointPorts.get("A"),
+                address = Option(InetAddress.getByName(clusterA.selfAddress.host.get))
+              ),
+              ResolvedTarget(
+                host = clusterB.selfAddress.host.get,
+                port = contactPointPorts.get("B"),
+                address = Option(InetAddress.getByName(clusterB.selfAddress.host.get))
+              ),
+              ResolvedTarget(
+                host = clusterC.selfAddress.host.get,
+                port = contactPointPorts.get("C"),
+                address = Option(InetAddress.getByName(clusterC.selfAddress.host.get))
+              )
+            )
+          )
+        )
+    )
+
+    "join three DNS discovered nodes by forming new cluster (happy path)" in {
+      val pA = TestProbe()(systemA)
+      pA.awaitAssert({
+        clusterA.state.members should have size(3)
+        clusterA.state.members.count(_.status == MemberStatus.Up) should === (3)
+      }, 20.seconds)
+    }
+
+    "terminate all systems" in {
+      try TestKit.shutdownActorSystem(systemA, 3.seconds)
+      finally try TestKit.shutdownActorSystem(systemB, 3.seconds)
+      finally TestKit.shutdownActorSystem(systemC, 3.seconds)
+    }
+
+  }
+
+}
