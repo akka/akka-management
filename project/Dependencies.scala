9b6ae046e20cede8a70dfc8833e18e48a03843ef
Ignasi Marimon-Clos <ignasi@lightbend.com>
2021-05-03 19:33:49 +0200

    Inline variable

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,9 +10,7 @@ object Dependencies {
   // Align the versions in integration-test/kubernetes-api-java/pom.xml
   val AkkaVersion = "2.6.14"
   val AkkaBinaryVersion = "2.6"
-  // Align the versions in integration-test/kubernetes-api-java/pom.xml
-  val AkkaHttp102 = "10.2.0"
-  val AkkaHttpVersion = AkkaHttp102
+  val AkkaHttpVersion = "10.2.0"
   val AkkaHttpBinaryVersion = "10.2"
 
   val ScalaTestVersion = "3.1.4"
