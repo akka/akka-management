642734ca2b20ea414cc8b410db8e9bc8876bc2b2
Ignasi Marimon-Clos <ignasi@lightbend.com>
2021-05-03 19:37:13 +0200

    Makes PemManagersProvider private

--- management-pki/src/main/scala/akka/pki/kubernetes/PemManagersProvider.scala
+++ management-pki/src/main/scala/akka/pki/kubernetes/PemManagersProvider.scala
@@ -17,6 +17,7 @@ import java.security.cert.X509Certificate
 import scala.concurrent.blocking
 
 import akka.annotation.ApiMayChange
+import akka.annotation.InternalApi
 import akka.pki.pem.DERPrivateKeyLoader
 import akka.pki.pem.PEMDecoder
 import javax.net.ssl.KeyManager
@@ -25,30 +26,19 @@ import javax.net.ssl.TrustManager
 import javax.net.ssl.TrustManagerFactory
 
 /**
+ * INTERNAL API
  * Convenience methods to ease building an SSLContext from k8s-provided PEM files.
  */
 // Duplicate from https://github.com/akka/akka/blob/31f654768f86db68f4c22daa2cbd0bae28fc1fad/akka-remote/src/main/scala/akka/remote/artery/tcp/ssl/PemManagersProvider.scala#L35
 // Eventually that will be a bit more open and we can reuse the class from akka in akka-management.
 // See also https://github.com/akka/akka-http/issues/3772
-@ApiMayChange
-object PemManagersProvider {
+@InternalApi
+private[akka] object PemManagersProvider {
 
-  def buildKeyManagers(privateKey: PrivateKey, cert: X509Certificate, cacert: Certificate): Array[KeyManager] = {
-    val keyStore = KeyStore.getInstance("JKS")
-    keyStore.load(null)
-
-    keyStore.setCertificateEntry("cert", cert)
-    keyStore.setCertificateEntry("cacert", cacert)
-    keyStore.setKeyEntry("private-key", privateKey, "changeit".toCharArray, Array(cert, cacert))
-
-    val kmf =
-      KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
-    kmf.init(keyStore, "changeit".toCharArray)
-    val keyManagers = kmf.getKeyManagers
-    keyManagers
-  }
-
-  def buildTrustManagers(cacert: Certificate): Array[TrustManager] = {
+  /**
+   * INTERNAL API
+   */
+  @InternalApi def buildTrustManagers(cacert: Certificate): Array[TrustManager] = {
     val trustStore = KeyStore.getInstance("JKS")
     trustStore.load(null)
     trustStore.setCertificateEntry("cacert", cacert)
@@ -59,7 +49,10 @@ object PemManagersProvider {
     tmf.getTrustManagers
   }
 
-  def loadPrivateKey(filename: String): PrivateKey = blocking {
+  /**
+   * INTERNAL API
+   */
+  @InternalApi def loadPrivateKey(filename: String): PrivateKey = blocking {
     val bytes = Files.readAllBytes(new File(filename).toPath)
     val pemData = new String(bytes, Charset.forName("UTF-8"))
     DERPrivateKeyLoader.load(PEMDecoder.decode(pemData))
@@ -67,7 +60,10 @@ object PemManagersProvider {
 
   private val certFactory = CertificateFactory.getInstance("X.509")
 
-  def loadCertificate(filename: String): Certificate = blocking {
+  /**
+   * INTERNAL API
+   */
+  @InternalApi def loadCertificate(filename: String): Certificate = blocking {
     val bytes = Files.readAllBytes(new File(filename).toPath)
     certFactory.generateCertificate(new ByteArrayInputStream(bytes))
   }
