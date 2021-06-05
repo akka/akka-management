2339ffe506f55955062a96c7c4df2721579073e7
Scala Steward <me@scala-steward.org>
2021-06-05 06:03:03 +0200

    wiremock-jre8 2.28.0 (was 2.27.2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -142,7 +142,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
     "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % Test,
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.28.0" % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % "it,test",
     "org.scalatestplus" %% "junit-4-13" % ScalaTestPlusJUnitVersion % "it,test",
     "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % "it,test"
