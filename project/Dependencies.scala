1e5aa0c2a85026b0b6aa711f8dfc9a940b04f71c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-07 10:31:06 +0100

    ecs 2.17.105 (was 2.16.104) (#997)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,7 +75,7 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-      "software.amazon.awssdk" % "ecs" % "2.16.104",
+      "software.amazon.awssdk" % "ecs" % "2.17.105",
       "org.scalatest" %% "scalatest" % ScalaTestVersion % Test
     ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
