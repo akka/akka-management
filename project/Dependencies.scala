dadf1ba9a1eeee9e8e5f6773fa619eef0a52bd00
RAtioNAn <72982136+RAtioNAn@users.noreply.github.com>
2022-01-10 10:56:34 +0200

    Support for log4j2 log levels (#973)
    * Log4j2
    
    * Small fixes.
    
    * Log4j2 version updated to 2.15.0.
    
    * Log4j2 updated to 2.16.0
    
    * Update project/project-info.conf
    
    Co-authored-by: Johan Andrén <johan@markatta.com>
    
    * Update project/project-info.conf
    
    Co-authored-by: Johan Andrén <johan@markatta.com>
    
    * Log4j2 updated to 2.17.0
    
    * Log4j2 version 2.17.2. Plugin disable.
    
    * Fix for isBlank method.
    
    Co-authored-by: Johan Andrén <johan@markatta.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,6 +20,8 @@ object Dependencies {
   val AwsSdkVersion = "1.12.134"
   val JacksonVersion = "2.11.4"
 
+  val Log4j2Version = "2.17.1"
+
   // often called-in transitively with insecure versions of databind / core
   private val JacksonDatabind = Seq(
     "com.fasterxml.jackson.core" % "jackson-databind" % JacksonVersion
@@ -102,6 +104,20 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
     "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
     "ch.qos.logback" % "logback-classic" % "1.2.10",
+    "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+    "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+    "org.scalatest" %% "scalatest" % ScalaTestVersion % Test,
+    "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % Test,
+    "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion % Test
+  )
+
+  val LoglevelsLog4j2 = Seq(
+    "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+    "org.apache.logging.log4j" % "log4j-core" % Log4j2Version,
+    "org.apache.logging.log4j" % "log4j-api" % Log4j2Version,
+    "org.apache.logging.log4j" % "log4j-slf4j-impl" % Log4j2Version,
     "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
     "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test,
