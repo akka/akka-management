9a77183f33bbdf576d347a21a51c81fd35d54fe7
KT0 <kto@evertrust.fr>
2023-08-09 13:34:01 +0200

    Add versioning info to be compatible

--- project/Common.scala
+++ project/Common.scala
@@ -81,7 +81,7 @@ object Common extends AutoPlugin {
       // -v Log "test run started" / "test started" / "test run finished" events on log level "info" instead of "debug".
       // -a Show stack traces and exception class name for AssertionErrors.
       testOptions += Tests.Argument(TestFrameworks.JUnit, "-v", "-a"),
-      scalaVersion := Dependencies.Scala212,
+//      scalaVersion := Dependencies.Scala212,
       sonatypeProfileName := "com.lightbend"
     )
 
---
+++ version.sbt
@@ -0,0 +1,3 @@
+ThisBuild / version := "1.1.4"
+ThisBuild / isSnapshot := false
+ThisBuild / scalaVersion := "2.13.8"
