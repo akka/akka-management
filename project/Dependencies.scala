9d2b81e1560d4f4f0e2beffd818b0f64a1e62b50
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-29 14:02:05 +0200

    bump: aws-java-sdk-autoscaling, ... 1.12.559 (was 1.12.539) (#1198)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val ScalaTestVersion = "3.2.17"
   val ScalaTestPlusJUnitVersion = ScalaTestVersion + ".0"
 
-  val AwsSdkVersion = "1.12.539"
+  val AwsSdkVersion = "1.12.559"
   val JacksonVersion = "2.15.2"
   val JacksonDatabindVersion = "2.15.2"
 
