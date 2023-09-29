d2f74e7acf0e66ac7cec00f8594f1be0cbff3dec
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-29 14:02:54 +0200

    bump: guava 32.0.1-jre (was 32.0.0-jre) (#1168)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
     "com.fasterxml.jackson.datatype" % "jackson-datatype-guava" % JacksonVersion,
     "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % JacksonVersion,
     // Specifying guava dependency because older transitive dependency has security vulnerability
-    "com.google.guava" % "guava" % "32.0.0-jre"
+    "com.google.guava" % "guava" % "32.0.1-jre"
   )
 
   val DiscoveryConsul = Seq(
