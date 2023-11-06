9b80350b00ed783540d9a838f82010157e38ce24
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-11-06 18:11:41 +0100

    bump: ecs 2.20.162 (was 2.20.158) (#1228)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -79,7 +79,7 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
       "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
-      ("software.amazon.awssdk" % "ecs" % "2.20.158").exclude("software.amazon.awssdk", "apache-client"),
+      ("software.amazon.awssdk" % "ecs" % "2.20.162").exclude("software.amazon.awssdk", "apache-client"),
       "org.scalatest" %% "scalatest" % ScalaTestVersion % Test
     ) ++ JacksonDatabind // aws-java-sdk depends on insecure version of jackson
 
