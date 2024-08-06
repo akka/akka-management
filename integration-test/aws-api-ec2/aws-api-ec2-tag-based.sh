5b296927c17864baec4aab5626c66dc2df4d59ea
Johan Andrén <johan@markatta.com>
2024-08-06 18:00:15 +0200

    build: Get rid of some final IntegrationTest deprecation warnings (#1292)
    * build: Get rid of some final IntegrationTest deprecation warnings

--- integration-test/aws-api-ec2/aws-api-ec2-tag-based.sh
+++ integration-test/aws-api-ec2/aws-api-ec2-tag-based.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Note: this seems completely outdated and is not used by CI
+
 echo Running integration tests for aws-api-ec2-tag-based
 echo You need to run this from the root folder of the akka-management project
 
@@ -17,6 +19,6 @@ sbt bootstrap-demo-aws-api-ec2-tag-based/universal:packageBin
 aws s3api create-bucket --bucket $BUCKET --region us-east-1
 aws s3 cp bootstrap-demo/aws-api-ec2/target/universal/app.zip s3://$BUCKET/$BUILD_ID/ --acl public-read
 # run the actual integration test
-sbt bootstrap-demo-aws-api-ec2-tag-based/it:test
+sbt bootstrap-demo-aws-api-ec2-tag-based/test
 # delete file (save a few cents)
 aws s3 rm s3://$BUCKET/$BUILD_ID/app.zip
