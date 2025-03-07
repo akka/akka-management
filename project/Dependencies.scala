9a995e26f11ffcfbbdf83b3e4b1a349bef289e96
Scala Steward <scala_steward@virtuslab.com>
2025-03-07 14:23:15 +0000

    bump: aws-java-sdk-ec2, aws-java-sdk-ecs 1.12.782 (was 1.12.779)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
   val ScalaTestPlusJUnitVersion = ScalaTestVersion + ".0"
 
   val AzureIdentityVersion = "1.15.0"
-  val AwsSdkVersion = "1.12.779"
+  val AwsSdkVersion = "1.12.782"
   val JacksonVersion = "2.17.2"
   val JacksonDatabindVersion = JacksonVersion
 
