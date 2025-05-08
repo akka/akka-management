72cc17b6af76cb2be7385c09a30547eba5cadde5
Scala Steward <scala_steward@virtuslab.com>
2025-05-08 21:36:03 +0000

    bump: azure-identity 1.16.0 (was 1.15.4)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   val ScalaTestVersion = "3.2.19"
   val ScalaTestPlusJUnitVersion = ScalaTestVersion + ".0"
 
-  val AzureIdentityVersion = "1.15.4"
+  val AzureIdentityVersion = "1.16.0"
   val AwsSdkVersion = "1.12.782"
   val JacksonVersion = "2.18.3"
   val JacksonDatabindVersion = JacksonVersion
