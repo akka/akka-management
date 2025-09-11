651c15fb86c99fd5b1ad502bdebe159d689a6691
Johan Andrén <johan@markatta.com>
2025-09-11 13:57:25 +0200

    chore: Automatic module names

---
+++ project/AutomaticModuleName.scala
@@ -0,0 +1,18 @@
+import sbt.*
+import sbt.Def
+import sbt.Keys.*
+
+/**
+ * Helper to set Automatic-Module-Name in projects.
+ *
+ * !! DO NOT BE TEMPTED INTO AUTOMATICALLY DERIVING THE NAMES FROM PROJECT NAMES !!
+ *
+ * The names carry a lot of implications and DO NOT have to always align 1:1 with the group ids or package names,
+ * though there should be of course a strong relationship between them.
+ */
+object AutomaticModuleName {
+  private val AutomaticModuleName = "Automatic-Module-Name"
+
+  def settings(name: String): Seq[Def.Setting[Task[Seq[PackageOption]]]] =
+    Seq(Compile / packageBin / packageOptions += Package.ManifestAttributes(AutomaticModuleName -> name))
+}
